ad40988a7ebbcd8031579cb909b985d480e1c241
Antoine Rousselle <antoine.rousselle@epitech.eu>
2025-08-27 21:22:59 -0400

    feat(App.jsx): added the basis file of the web site

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import { Toaster } from "./components/ui/toaster";
+import { ModeProvider } from "./context/ModeContext";
+
+function App() {
+  return (
+    <div className="min-h-dvh bg-background text-foreground">
+      <ModeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Portfolio />} />
+            <Route path="/portfolio" element={<Portfolio initialMode="portfolio" />} />
+          </Routes>
+          <Toaster />
+        </BrowserRouter>
+      </ModeProvider>
+    </div>
+  );
+}
+
+export default App;
